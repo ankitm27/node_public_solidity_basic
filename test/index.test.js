5173d2b9949bd8ecab889718f804320572127027
ankitm27 <anki.malhotra00@gmail.com>
2019-07-07 12:30:38 +0530

    deafult cases added

--- test/index.test.js
+++ test/index.test.js
@@ -5,7 +5,7 @@ const ganache = require('ganache-cli')
 const Web3 = require('web3')
 
 const web3 = new Web3(ganache.provider())
-const { interface,byteCode } = require("../compile");
+const { interface,bytecode } = require("../compile");
 
 let accounts;
 let inbox;
@@ -14,7 +14,7 @@ beforeEach(async() => {
     accounts = await web3.eth.getAccounts()
     inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
-      data: byteCode,
+      data: bytecode,
       arguments: ['check']
     })
     .send({ from: accounts[0], gas: '1000000' });
@@ -22,7 +22,13 @@ beforeEach(async() => {
 
 describe('Inbox',() => {
     it('deploys a contract', () => {
-        console.log("accounts",accounts)
-        console.log("inbox",inbox)
+        // console.log("accounts",accounts)
+        // console.log("inbox",inbox)
+        assert.ok(inbox.options.address)
+    })
+
+    it('has a default message', async() => {
+        const message = await inbox.methods.message().call()
+        assert.equal(message,'check')
     })
 })
