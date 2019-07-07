24a4672d685deca06b6902cdc8a7cd8087646fe8
ankitm27 <anki.malhotra00@gmail.com>
2019-07-07 11:56:05 +0530

    compile code added

--- test/index.test.js
+++ test/index.test.js
@@ -5,15 +5,24 @@ const ganache = require('ganache-cli')
 const Web3 = require('web3')
 
 const web3 = new Web3(ganache.provider())
+const { interface,byteCode } = require("../compile");
 
 let accounts;
+let inbox;
+
 beforeEach(async() => {
     accounts = await web3.eth.getAccounts()
-    
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: byteCode,
+      arguments: ['check']
+    })
+    .send({ from: accounts[0], gas: '1000000' });
 })
 
 describe('Inbox',() => {
     it('deploys a contract', () => {
         console.log("accounts",accounts)
+        console.log("inbox",inbox)
     })
 })
